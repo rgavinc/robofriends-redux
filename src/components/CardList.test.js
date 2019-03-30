cc616848eb602e0c9d6402f2309765d916207438
Personal <personal@Gavins-MacBook-Pro.local>
2019-03-30 14:25:18 -0600

    added tests and such

---
+++ src/components/CardList.test.js
@@ -0,0 +1,10 @@
+import { shallow } from "enzyme";
+import React from "react";
+import CardList from "./CardList";
+
+it("expect to render CardList Component", () => {
+  const mockRobots = [
+    { id: 1, name: "John Snow", username: "JohnJohn", email: "john@gmail.com" }
+  ];
+  expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+});
